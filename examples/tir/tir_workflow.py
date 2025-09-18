2a368087bdfec71b7f3be8e0ff67715d93ceeee9
maojianbo <maojianbo@bytedance.com>
2025-09-18 04:59:48 +0000

    fix: rm sleep

--- examples/tir/tir_workflow.py
+++ examples/tir/tir_workflow.py
@@ -201,7 +201,8 @@ class TIRWorkflow(RolloutWorkflow):
                 turn += 1
                 has_tool = True
                 tool_call_count += 1  # Increment tool call count
-                tool_success_count += 1 if tool_status else 0
+                if tool_status == ToolCallStatus.SUCCESS and "Error" not in tool_results:
+                    tool_success_count += 1
                 tool_results = self._process_tool_result(tool_results)
                 # Append tool response token IDs
                 tool_rsp_token_ids=self.tokenizer.encode(tool_results, add_special_tokens=False)
