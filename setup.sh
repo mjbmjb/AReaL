e1d0e38ced4815c6f7789cdc7050da76e515e5cb
Mao <maojianbo@bytedance.com>
2025-09-18 13:08:08 +0800

    clean file

--- setup.sh
+++
@@ -1,8 +0,0 @@
-bash /mnt/bn/yufei1900/maojianbo/workspace/Miniforge3-Linux-x86_64.sh  -b -u -p ~/miniforge3
-source ~/miniforge3/bin/activate
-conda create -n areal python=3.12
-conda activate areal
-bash examples/env/setup-pip-deps.sh
-pip install qwen_agent -y
-pip uninstall wandb -y
-pip install -U byted-wandb -i https://bytedpypi.byted.org/simple
